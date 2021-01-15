e3cc064cd377f6ceb5460ff6ab2556364e57ab09
Angelo Marques <anngelo.emanuel@hotmail.com>
2021-01-14 22:42:20 -0300

    building the header and the sidebar

---
+++ src/firebase.js
@@ -0,0 +1,11 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyBpD_-YsB23SXsdSK1_Nk9HqqIpaBo9STI",
+  authDomain: "facebook-clone-d5a56.firebaseapp.com",
+  databaseURL: "https://facebook-clone-d5a56-default-rtdb.firebaseio.com",
+  projectId: "facebook-clone-d5a56",
+  storageBucket: "facebook-clone-d5a56.appspot.com",
+  messagingSenderId: "1051067286759",
+  appId: "1:1051067286759:web:12a54d1e72f1eba0248e42",
+  measurementId: "G-RXEPXNKZJD",
+};
